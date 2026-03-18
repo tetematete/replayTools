f6cd8df4a338c134093c44bca370df1026b02eb7
Tete Soto <mateusxsoto@gmail.com>
2026-03-17 20:11:35 -0400

    dont even remember it works great tho

--- replayTools.lua
+++ replayTools.lua
@@ -4,6 +4,7 @@ local resultsPage = nil
 local resultsFile = JSON.parse(ac.load("resultsFile"))
 local page = 0
 local resultsFilepath = ""
+local server = 0
 local settings = ac.storage{
   URL = "http://162.19.220.231:8772"
 }
@@ -14,31 +15,40 @@ function script.windowMain(dt)
   ui.tabBar("Replay Tools", function()
 
 
-    ui.tabItem("Replay Data", function ()
+    ui.tabItem("Replay Data", function()
       ui.tabBar("ReplayControls", function()
-        ui.tabItem("Events",function ()
-                        ac.debug("frame", sim.replayCurrentFrame)
-              ac.debug("total", sim.replayFrames)
-          ui.childWindow("Event Table", vec2(500,500),function()
-          
-          if resultsFile ~= nil then
-          local replayStartTime = (resultsFile["Laps"][1]["Timestamp"] - (resultsFile["Laps"][1]["LapTime"]/1000))-resultsFile["SessionConfig"]["wait_time"]
-          --ac.log(replayStartTime)
-          ac.debug("Events", resultsFile["Laps"])
-          for index, value in ipairs(resultsFile["Events"]) do
-          if value["Driver"]["Name"] ~= resultsFile["Events"][math.min(index+1,#resultsFile["Events"])]["OtherDriver"]["Name"] then
-            if ui.button(ac.lapTimeToString(((value["Timestamp"]-replayStartTime-1)-((value["Timestamp"]-replayStartTime-1)*0.0013))*1000).." " ..value["Driver"]["Name"] .. " " .. value["Type"] .. " " .. value["OtherDriver"]["Name"]) then
-              --ac.log(((value["Timestamp"]-replayStartTime-1)-((value["Timestamp"]-replayStartTime-1)*0.004)*1000)/sim.replayFrameMs)
-              ac.setReplayPosition((((value["Timestamp"]-replayStartTime-2)-((value["Timestamp"]-replayStartTime-2)*0.0013))*1000)/sim.replayFrameMs,1)
-              ac.focusCar(math.max(ac.getCarByDriverName(value["Driver"]["Name"]),(ac.getCarByDriverName(value["OtherDriver"]["Name"]))))
+        ui.tabItem("Events", function()
+          ac.debug("frame", sim.replayCurrentFrame)
+          ac.debug("total", sim.replayFrames)
+          ui.columns(2, true, "columnlayout")
+          ui.childWindow("Event Table", vec2(500, 500), function()
+            if resultsFile ~= nil then
+              local replayStartTime = (resultsFile["Laps"][1]["Timestamp"] - (resultsFile["Laps"][1]["LapTime"] / 1000)) -
+              resultsFile["SessionConfig"]["wait_time"]
+              --ac.log(replayStartTime)
+              ac.debug("Events", resultsFile["Laps"])
+              for index, value in ipairs(resultsFile["Events"]) do
+                if value["Driver"]["Name"] ~= resultsFile["Events"][math.min(index + 1, #resultsFile["Events"])]["OtherDriver"]["Name"] then
+                  if ui.button(ac.lapTimeToString(((value["Timestamp"] - replayStartTime - 1) - ((value["Timestamp"] - replayStartTime - 1) * 0.0013)) * 1000) .. " " .. value["Driver"]["Name"] .. " " .. ((value["Type"] == "COLLISION_WITH_CAR" and "with") or (value["Type"] == "COLLISION_WITH_ENV" and "With Environment. lmao.")) .. " " .. value["OtherDriver"]["Name"]) then
+                    --ac.log(((value["Timestamp"]-replayStartTime-1)-((value["Timestamp"]-replayStartTime-1)*0.004)*1000)/sim.replayFrameMs)
+                    ac.setReplayPosition(
+                    (((value["Timestamp"] - replayStartTime - 2) - ((value["Timestamp"] - replayStartTime - 2) * 0.0013)) * 1000) /
+                    sim.replayFrameMs, 1)
+                    ac.focusCar(math.max(ac.getCarByDriverName(value["Driver"]["Name"]),
+                      (ac.getCarByDriverName(value["OtherDriver"]["Name"]))))
+                  end
+                end
+              end
             end
+          end)
+          ui.nextColumn()
+          for index, value in ipairs(resultsFile["Events"]) do
+            
           end
-          end
-        end
-        end)
-        end)--events tab
-      end)--Replay Controls Tab Bar
-    end)--Replay Data Tab
+          ui.columns(1, true, "columnlayout")
+        end) --events tab
+      end)   --Replay Controls Tab Bar
+    end)     --Replay Data Tab
 
 
 
@@ -46,6 +56,7 @@ function script.windowMain(dt)
       if ui.button("Autofill") then
         searchQuery = "+" .. ac.getTrackID()
       end
+
       local searchQuery, searchChanged, searchConfirm = ui.inputText("Search for results", searchQuery,
         ui.InputTextFlags.RetainSelection)
       local searchConfirmButton = ui.button("Search Results")
@@ -58,7 +69,7 @@ function script.windowMain(dt)
         ui.childWindow("Results List", vec2(400, 300), function()
           if resultsPage["results"] ~= nil then
             for index, v in ipairs(resultsPage["results"]) do
-              if ui.menuItem(isoTimetoReadable(v["date"]) .. " | " .. v["session_type"] .. " at " .. v["track"], selectedResults == index) then
+              if ui.menuItem(isoTimetoReadable(v["date"]) .. " | " .. (v["session_type"]) .. " at " .. v["track"], selectedResults == index) then
                 selectedResults = index
                 resultsFilepath = v["results_json_url"]
               end
@@ -96,11 +107,11 @@ if input == nil then
 end
   local sort = "date"
 
-  web.get(settings.URL .. "/api/results/list.json?q=" .. query .. "&sort=" .. sort .. "&page=" .. pageNumber, function(err, response)
+  web.get(settings.URL .. "/api/results/list.json?q=" .. query .. "&sort=" .. sort .. "&page=" .. pageNumber .. "&server=" .. server, function(err, response)
     ac.debug("error", error)
     resultsPage = JSON.parse(response.body)
   end)
-  ac.log(settings.URL .. "/api/results/list.json?q=" .. query .. "&sort=" .. sort .. "&page=" .. page)
+  ac.log(settings.URL .. "/api/results/list.json?q=" .. query .. "&sort=" .. sort .. "&page=" .. pageNumber .. "&server=" .. server)
 end
 
 function loadResults(file)
